02366b00bf93b0d4ac2adb1f42528ba671b74c29
root <root@ip-172-31-47-165.us-west-2.compute.internal>
2017-05-24 15:33:01 +0000

    Lets hope this works.

--- dctcp-assignment/dctcp/run-dctcp.sh
+++ dctcp-assignment/dctcp/run-dctcp.sh
@@ -38,7 +38,7 @@ function tcp {
 	odir=tcp-n$n-bw$bw
 	sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n
 	sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
-	sudo python ../util/plot_queue.py --maxy 150 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+	sudo python ../util/plot_queue.py --miny 150 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
 	sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
@@ -56,7 +56,7 @@ function dctcp {
     odir=dctcp-n$n-bw$bw
 	sudo python dctcp.py --bw $bw --maxq $maxq --dir $odir -t $t -n $n --dctcp
 	sudo python ../util/plot_rate.py --maxy $bw -f $odir/txrate.txt -o $odir/rate.png
-	sudo python ../util/plot_queue.py --maxy 150 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
+	sudo python ../util/plot_queue.py --miny 150 -f $odir/qlen_s1-eth1.txt -o $odir/qlen.png
 	sudo python ../util/plot_tcpprobe.py -f $odir/tcp_probe.txt -o $odir/cwnd.png
 }
 
